21d4b91eafc24a74f0f44842d5f2f2bf6f26ebe8
Krishna Sarkar <108187693+krishnaSarkar5@users.noreply.github.com>
2023-09-03 10:58:29 +0530

    Update build-array-from-permutation.java

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -9,3 +9,25 @@ class Solution {
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int[] buildArray(int[] nums) {
+//         isPerm(nums, 0);
+//         System.gc();
+//         return nums;
+
+//     }
+
+//     public void isPerm(int[] nums, int start)
+//     {
+//         if(start < nums.length)
+//         {
+//             int temp = nums[start];
+//             int result = nums[temp];
+//             isPerm(nums, start+1);
+//             nums[start] = result;
+//         }
+//     }
+    
+// }
