e97e7f16011ad0cb5809c939740a35856893ab83
Krishna Sarkar <108187693+krishnaSarkar5@users.noreply.github.com>
2023-10-26 11:24:03 +0530

    Time: 8 ms (40.37%) | Memory: 45.3 MB (12.56%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,25 +1,70 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         
-        int l = 0, h = arr.length - 1;
+       int n = arr.length;
 
-        while (h - l >= k) {
+        int h = lowerBound(arr, x);
 
-            if ((x - arr[l]) > (arr[h] - x)) {
-                l++;
+        System.out.println("+++++++ " + h);
+
+        int l = h - 1;
+
+        while (k > 0) {
+
+            if (l < 0) {
+                h++;
+            } else if (h >= n) {
+                l--;
+            } else if ((x - arr[l]) > (arr[h] - x)) {
+                h++;
             } else {
-                h--;
+                l--;
             }
-
+            k--;
         }
 
         List<Integer> res = new ArrayList<>();
 
-        for (int i = l; i <= h; i++) {
+        for (int i = l + 1; i < h; i++) {
             res.add(arr[i]);
         }
 
         return res;
 
     }
+
+
+
+    public static int lowerBound(int[] arr, int target) {
+
+        int s = 0, e = arr.length - 1;
+
+        int ans = 0;
+
+        if (target >= arr[arr.length - 1]) {
+            return e;
+        } else if (target <= arr[0]) {
+            return 0;
+        }
+
+        while (s <= e) {
+
+            int m = s + (e - s) / 2;
+
+            if (target <= arr[m]) {
+                ans = m;
+                e = m - 1;
+            } else if (target > arr[m]) {
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+
+        }
+
+        return ans;
+    }
+
+
+
 }
