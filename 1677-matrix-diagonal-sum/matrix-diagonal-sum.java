6e58378cf1085bd0fdc5f6452777a7b1e62494e8
Krishna Sarkar <108187693+krishnaSarkar5@users.noreply.github.com>
2023-10-27 22:36:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (13.27%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -5,13 +5,21 @@ class Solution {
         int sum=0;
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
 
-                if(i==j || (i+j)==n-1){
-                    sum += mat[i][j];
-                }
+            sum += mat[i][i];
+            int j = n-1-i;
+            if(i!=j)
+                sum += mat[i][j];
 
-            }
+
+
+            // for(int j=0;j<n;j++){
+
+            //     if(i==j || (i+j)==n-1){
+            //         sum += mat[i][j];
+            //     }
+
+            // }
         }
         return sum;
 
